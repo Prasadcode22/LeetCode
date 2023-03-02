a5017e1061198c635967eef69fde501b4bff65ff
Prasad Aher <aherprasad1212@gmail.com>
2023-03-02 23:34:17 +0530

    Leetcode 443 Solution

---
+++ 443_string_compression.py
@@ -0,0 +1,97 @@
+
+class Solution:
+    def compress(chars):
+        chars.sort()
+        out = []
+        for i in chars:
+            p = chars.count(i)
+            if i not in out:
+                if p > 9 :
+                    a = p // 9
+                    b = p%9
+                    for j in range(a):
+                        out.append(i)
+                        out.append('9')
+                    out.append(i)
+                    out.append(str(b))
+                else:
+                    out.append(i)
+                    out.append(str(p))
+        return len(out),out
+
+if __name__ == '__main__':
+    chars =["a","a","b","b","c","c","c"]
+    print(Solution.compress(chars))
+    
+# class Solution:
+#     def compress(chars):
+#         out = []
+#         n = 0
+#         for i in chars:
+#             if i not in out:
+#                 out.append(i)
+#                 n += 1
+#             for i in out:
+#                 n += 1
+               
+
+#         return(len(out), out)
+
+# class Solution:
+#     def compress(chars):
+#         out = []
+#         n = 0
+#         for i in chars:
+#             if i not in out:
+#                 out.append(i)
+#                 n += 1
+#             for i in out:
+#                 n += 1
+               
+
+#         return(len(out), out)
+
+
+# def compress(chars):
+# chars.sort()
+# out = []
+# for i in chars:
+#     p = chars.count(i)
+#     if i not in out:
+#         if p > 9 :
+#             a = p // 9
+#             b = p%9
+#             for j in range(a):
+#                 out.append(i)
+#                 out.append('9')
+#             out.append(i)
+#             out.append(str(b))
+#         else:
+#             out.append(i)
+#             out.append(str(p))
+# return len(out),out
+
+
+# class Solution:
+#     def compress(self, chars: List[str]) -> int:
+#         chars.sort()
+#         out = []
+#         for i in chars:
+#             p = chars.count(i)
+#             if i not in out:
+#                 if p > 9 :
+#                     a = p // 9
+#                     b = p%9
+#                     for j in range(a):
+#                         out.append(i)
+#                         out.append('9')
+#                     out.append(i)
+#                     out.append(str(b))
+#                 else:
+#                     out.append(i)
+#                     out.append(str(p))
+#         print(out)
+
+
+# if __name__ == '_main__':
+#     char
