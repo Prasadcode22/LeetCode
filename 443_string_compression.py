f54e6618ed3a85c6a72b92ec7707258c0d14b8bd
Prasad Aher <aherprasad1212@gmail.com>
2023-03-03 00:16:55 +0530

    Leetcode 443 Solution

--- 443_string_compression.py
+++ 443_string_compression.py
@@ -6,23 +6,26 @@ class Solution:
         for i in chars:
             p = chars.count(i)
             if i not in out:
-                if p > 9 :
-                    a = p // 9
-                    b = p%9
-                    for j in range(a):
-                        out.append(i)
-                        out.append('9')
+                if p == 1:
                     out.append(i)
-                    out.append(str(b))
+                elif p > 9 :
+                    a = p // 10
+                    b = p%10
+                    out.append(i)
+                    out.append(str(a))
+                    if b > 0:
+                        out.append(str(b))
                 else:
                     out.append(i)
                     out.append(str(p))
         return len(out),out
 
 if __name__ == '__main__':
-    chars =["a","a","b","b","c","c","c"]
+    # chars =["a","a","b","b","c","c","c"]
+    # chars =["a","a","b","b","b","b","b","b","b","b","b","b","b","b","c","c","c"]
+    # chars = ['a']
     print(Solution.compress(chars))
-    
+
 # class Solution:
 #     def compress(chars):
 #         out = []
