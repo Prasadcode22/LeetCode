55cdbd59d9ddd72b1c703796d0eee8cb299c0d6d
Prasad Aher <aherprasad1212@gmail.com>
2023-03-06 00:05:52 +0530

    20 Valid Parenthesis

--- 20_Valid_Parenthesis.py
+++ 20_Valid_Parenthesis.py
@@ -1,17 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # creating dict for cheching the parenthesis follows as per rules
         par = {')':'(',']':'[','}':'{'}
+        # stack data structure used for pop and push operation
         stack =[]
         for c in s:
             if c in par :
                 if stack and stack[-1] == par[c]:
-                    stack.pop()
+                    # check the top of the stack element at stack[-1]
+                    stack.pop() # pop the parenthesis 
                 else:
-                    return False
+                    return False # which means the parenthesis are in wrong order 
             else:
-                stack.append(c)
-        return True if not stack else False
+                stack.append(c) # push into the stack till the closing type of parenthesis doesn't occurs for its similar 
+        return True if not stack else False # return true if the stack is empty or else false 
 
+# main Function 
 if __name__ =='__main__':
-    s = input()
+    s = input # User input as str
     print(Solution().isValid(s))
