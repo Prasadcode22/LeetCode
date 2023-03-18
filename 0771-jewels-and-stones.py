64163b96290a8de912cfd7d402c018bfc69e8414
Prasad Aher <aherprasad1212@gmail.com>
2023-03-18 23:53:55 +0530

    0771_numJewelsInStones

--- 0771-jewels-and-stones.py
+++ 0771-jewels-and-stones.py
@@ -2,3 +2,6 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         return sum(i in jewels for i in stones)
     
+if __name__ == '__main__':
+    jewels, stones = "aA","aAAbbbb"
+    print(Solution().numJewelsInStones(jewels,stones))
