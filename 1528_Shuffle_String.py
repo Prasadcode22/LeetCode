61368d8f8e6b81f68bf0ba466df5b3edbd810ea7
Prasad Aher <aherprasad1212@gmail.com>
2023-03-20 22:10:28 +0530

    1528_Shuffle_String

---
+++ 1528_Shuffle_String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def restoreString(self, s, indices):
+        out = ['']*len(s)
+        for index, char in enumerate(s):
+            out[indices[index]]= char
+        return ''.join(out)
+    
+if __name__ == '__main__':
+    s = "codeleet"
+    indices = [4,5,6,7,0,2,1,3]
+    print(Solution().restoreString(s, indices))
